6c116458ccfde8fd8ed50af88d88b23a880e01f4
TheRedstoneDaim <joeljacob500@gmail.com>
2018-10-25 19:10:32 -0400

    Create YTCommentPrank.py

---
+++ YTCommentPrank.py
@@ -0,0 +1,16 @@
+import random
+
+for i in range(1000):
+    number = random.randint(0, 12)
+    if number == 4:
+        print("Your regretting this aren't you?")
+    if number == 6:
+        print("How does it feel to have ur time wasted ;)")
+    if number == 8:
+        print("Hope your not using limited mobile data...")
+    if number == 10:
+        print("Your almost to the end (or not)")
+    if number == 11:
+        print("My computer lagged while generating this, if that makes u feel better...")
+    else:
+        print("....................................................................")
